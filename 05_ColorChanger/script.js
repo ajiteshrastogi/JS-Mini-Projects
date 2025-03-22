298d6aeb92fb8b5f7ec6b6ad8bcfd9c897b54d85
ajiteshrastogi <ajiteshrastogi19@gmail.com>
2025-03-22 20:34:15 +0530

    Constantly Change Your Bg

--- 05_ColorChanger/script.js
+++ 05_ColorChanger/script.js
@@ -1,17 +1,20 @@
 const color = () => {
     const hex = "0123456789ABCDEF";
     let out = "#";
+
     for(let i = 0; i<6;i++){
         out += hex[Math.floor(Math.random() *16)]
         console.log(out);
     }
     return out;
 };
+
 const body = document.querySelector('body');
 let reference;
 function startChanging(){
     body.style.backgroundColor = color();
 };
+
 document.querySelector('#start').addEventListener('click', function(){
     reference = setInterval(startChanging,1000);
 });
